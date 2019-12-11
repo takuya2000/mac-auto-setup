35b69681e55caab145bb04efce7c108b439c50a7
takuya2000 <takuya.eng@gmail.com>
2019-12-11 10:39:55 +0900

    Update app.sh

--- app.sh
+++ app.sh
@@ -12,8 +12,18 @@ EOS
 # Install web apps.
 #
 echo " ----- Install web apps ------"
-brew cask install java
+brew cask install bettertouchtool
+brew cask install karabiner-elements
+brew cask install google-chrome
+brew cask install flux
+brew cask install iterm2
+
+brew install youtube-dl
+brew install mpv --with-bundle
 brew cask install atom
+brew cask install hyperswitch
+brew cask install macmediakeyforwarder
+
 #brew install mysql
 #brew cask install shifty
 #brew cask install chromedriver
@@ -26,21 +36,17 @@ brew cask install atom
 #brew cask install eclipse-java
 #brew cask install firefox
 #brew cask install genymotion
-brew cask install google-chrome
-brew cask install iterm2
+
 #brew cask install postman
-brew install youtube-dl
-brew install mpv --with-bundle
+
 #brew install tmux
 #brew install the_silver_searcher
 brew cask install fluid
-brew cask install hyperswitch
 brew cask install skitch
-brew cask install bettertouchtool
-brew cask install karabiner-elements
-brew cask install flux
 brew cask install alfred
-brew cask install macmediakeyforwarder
+
 
 brew install mas
 mas install 539883307
+
+brew cask install java
