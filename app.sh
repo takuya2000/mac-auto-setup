6407157e02e95bec0806a6089c4d421fd7ab6d95
takuya2000 <takuya.eng@gmail.com>
2020-12-27 09:15:37 +0900

    Update app.sh

--- app.sh
+++ app.sh
@@ -12,52 +12,48 @@ EOS
 # Install web apps.
 #
 echo " ----- Install web apps ------"
-brew cask install karabiner-elements
-brew cask install bettertouchtool
-brew cask install google-chrome
+brew install --cask flux
+brew install --cask java
+brew install --cask karabiner-elements
+brew install --cask bettertouchtool
+brew install --cask google-chrome
+brew install --cask iterm2
+brew install --cask mpv
+brew install --cask iina
+brew install --cask atom
+brew install --cask hyperswitch
+brew install --cask macmediakeyforwarder
+brew install --cask alfred
+brew install --cask makemkv
+brew install --cask selfcontrol
+brew install --cask zoomus
+brew install --cask slack
+brew install --cask background-music
 
-brew cask install flux
-brew cask install iterm2
-
-brew install youtube-dl
-brew cask install mpv
-brew cask install iina
-brew cask install atom
-brew cask install hyperswitch
-brew cask install macmediakeyforwarder
 
 #brew install mysql
-#brew cask install shifty
-#brew cask install chromedriver
-
-#brew cask install cheatsheet
-#brew cask install cyberduck
-#brew cask install docker
-#brew cask install dropbox
-#brew cask install duet
-#brew cask install eclipse-java
-#brew cask install firefox
-#brew cask install genymotion
-
-#brew cask install postman
-
 #brew install tmux
 #brew install the_silver_searcher
-#brew cask install fluid
-#brew cask install skitch
-brew cask install alfred
-brew cask install makemkv
-brew cask install selfcontrol
-brew cask install zoomus
-brew cask install slack
+#brew install --cask shifty
+#brew install --cask chromedriver
+#brew install --cask cheatsheet
+#brew install --cask cyberduck
+#brew install --cask docker
+#brew install --cask dropbox
+#brew install --cask duet
+#brew install --cask eclipse-java
+#brew install --cask firefox
+#brew install --cask genymotion
+#brew install --cask postman
+#brew install --cask fluid
+#brew install --cask skitch
 
 brew install mas
 mas install 539883307
 mas install 1142151959
 mas install 439654198
 
-brew cask install java
 brew install ffmpeg
 brew install cliclick
-brew cask install  background-music
 brew install streamlink
+brew install youtube-dl
