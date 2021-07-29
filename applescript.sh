60323224c0b55c33ba11f8d410107d57d9043532
takuya2000 <takuya.eng@gmail.com>
2021-07-29 15:58:12 +0900

    Update applescript.sh

--- applescript.sh
+++ applescript.sh
@@ -4,7 +4,7 @@
   osascript -e "
 tell application \"System Preferences\"
 	activate
-	reveal anchor \"shortcutsTab" of pane id \"com.apple.preference.keyboard\"
+	reveal anchor \"shortcutsTab\" of pane id \"com.apple.preference.keyboard\"
 end tell
 "
 
